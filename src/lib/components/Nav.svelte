17ae4c235b6e6b70f993843a258eba57303644bf
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-12-29 01:05:20 +0800

    add

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,6 +1,63 @@
+
+
+
 <nav>
-	<a href="/">home</a>
-	<a href="/collection">collection</a>
-    <a href="/generate">generate</a>
-    <a href="/play">play</a>
+    <div id="content-wrapper">
+    <div class="nav-left">
+	    <a href="/">Home</a>
+	    <a href="/collection">Collection</a>
+        <a href="/generate">Generate</a>
+    </div>
+    <div class="nav-right">
+        <p>|</p>
+        <a href="/play">Play</a>
+    </div>
+</div>
 </nav>
+
+
+<style>
+
+    #content-wrapper {
+        padding-left: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: left;
+        align-items: center;
+        gap: 100px;
+        font-family:Helvetica, sans-serif;
+        height: 80px;
+        
+    }
+
+    nav {
+        display: flex;
+        background-color: #6f4b25;
+        margin: auto;
+        padding: 0px;
+        color: white;
+        font-size: 25px;
+        align-items: center;
+
+        
+    }
+    a {
+        color: white;
+        text-decoration: none;
+    }
+
+    .nav-left {
+        display: flex;
+        flex: 1;
+        gap: 30px;
+    }
+
+    .nav-right {
+        display: flex;
+        flex-direction: row;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+    }
+</style>
