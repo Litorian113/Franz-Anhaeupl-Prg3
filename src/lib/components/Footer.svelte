a4f8c08f56dd97473e95e77acee4c31322b7844f
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-12-29 01:05:12 +0800

    add

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,67 @@
+
+
+
+<footer>
+<div class="footer-section">
+    <div class="footer-left-section">
+        <ul class="bigFoot">
+            <li>Impressum</li>
+            <li>Datenschutz</li>
+        </ul>
+    </div>
+    <div class="footer-right-section">
+        <p>Copyright 2024</p>
+    </div>
+</div>
+</footer>
+
+
+<style>
+    footer {
+    height: 200px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    background-color: rgb(35, 35, 35);
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid #fff;
+    font-family: Helvetica, sans-serif;
+    color: white;
+}
+
+.footer-section {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+
+.footer-left-section {
+    display: flex;
+    flex-direction: row;
+}
+
+.footer-right-section {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.bigFoot {
+    display: flex;
+    gap: 20px;
+    list-style-type: none; 
+}
+
+
+@media (max-width: 550px) {
+
+    .footer-section {
+        gap: 0px;
+    }
+}
+</style>
