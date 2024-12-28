75eb010fee65b35cf043200052a84f092e1d0617
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-12-29 01:05:16 +0800

    add

---
+++ src/lib/components/ImgBoxSection.svelte
@@ -0,0 +1,83 @@
+<script>
+
+
+</script>
+
+
+<div class="heading-section">
+
+    <h1>die welt von quattera</h1>
+</div>
+
+<div class="infocard-section">
+    <div class="infocard1">
+        <img src="../vectors/icon1.svg" class="bild" alt="Icon eins">
+        <h3>fantastisches universum von quatteria</h3>
+        <p>
+            tauche ein in das fantastische universum von quatteria und entdecke legendäre Karten, die nur auf dich
+            warten!
+        </p>
+    </div>
+
+    <div class="infocard1">
+        <img src="../vectors/icon2.svg" class="bild" alt="Icon eins">
+        <h3>erkunde neue orte</h3>
+        <p>
+            begib dich auf eine reise und entdecke geheimnisvolle orte voller mächtiger karten. jeder ort birgt neue
+            herausforderungen!
+        </p>
+    </div>
+
+    <div class="infocard1">
+        <img src="../vectors/icon3.svg" class="bild" alt="Icon eins">
+        <h3>eigene karten erstellen</h3>
+        <p>
+            bald verfügbar: erstelle deine eigenen karten und erweitere das universum von quatteria mit deiner
+            kreativität!
+        </p>
+    </div>
+
+</div>
+
+
+<style>
+
+.heading-section {
+    display: flex;
+    flex-direction: row;
+    height: 150px;
+    justify-content: center;
+    align-items: center;
+}
+
+.infocard-section {
+    display: flex;
+    flex-direction: row;
+    height: auto;
+    justify-content: center;
+    align-items: center;
+    gap: 40px;
+    flex-wrap: wrap;
+    padding-bottom: 110px;
+}
+
+.infocard1 {
+    display: flex;
+    flex-direction: column;
+    height: 400px;
+    width: 300px;
+    align-items: center;
+}
+
+.bild {
+    display: flex;
+    width: 50%;
+    padding-bottom: 40px;
+}
+
+h3 {
+    color: #FFE81F;
+    width: 100%;
+}
+
+</style>
