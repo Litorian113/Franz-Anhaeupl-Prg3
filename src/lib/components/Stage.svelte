c0d1abe7a5f3ca1ec00bbc936d2bc1cc8da96395
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-12-29 01:05:23 +0800

    add

---
+++ src/lib/components/Stage.svelte
@@ -0,0 +1,18 @@
+
+
+<div class="parallax">
+
+</div>
+
+
+<style>
+.parallax {
+    background-image: url('../images/Background.webp'); /* Pfad zum Bild */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 500px;
+}
+
+</style>
