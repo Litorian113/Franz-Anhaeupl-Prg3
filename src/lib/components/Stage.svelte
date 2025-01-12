5158ad6d5abc26c593495d6f8591fe171f80d09a
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2025-01-12 11:25:23 +0100

    Änderung des Parallax-Elements in ein Banner mit aktualisiertem Hintergrundbild und responsiven Höhenanpassungen für verschiedene Bildschirmgrößen

--- src/lib/components/Stage.svelte
+++ src/lib/components/Stage.svelte
@@ -1,18 +1,29 @@
 
 
-<div class="parallax">
+<div class="banner">
 
 </div>
 
 
 <style>
-.parallax {
-    background-image: url('../images/Background.webp'); /* Pfad zum Bild */
-    background-attachment: fixed;
-    background-position: center;
+.banner {
+    background-image: url('../images/stageCover.jpg'); /* Pfad zum Bild */
     background-repeat: no-repeat;
-    background-size: cover;
-    height: 500px;
+    background-size: cover; /* Deckt die gesamte Breite und Höhe ab, ohne Verzerrungen */
+    background-position: center; /* Zentriert das Bild */
+    width: 100%;
+    height: 650px; /* Standardhöhe für das Banner */
 }
 
+@media (max-width: 768px) {
+    .banner {
+        height: 300px; /* Kleinere Höhe für mobile Geräte */
+    }
+}
+
+@media (max-width: 480px) {
+    .banner {
+        height: 200px; /* Noch kleinere Höhe für sehr kleine Bildschirme */
+    }
+}
 </style>
